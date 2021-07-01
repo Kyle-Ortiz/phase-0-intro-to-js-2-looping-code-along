3afb4b5a0ebd0c632f0cfecd08476e161c7f2f00
Kyle Ortiz <kyleaortiz@gmail.com>
2021-07-01 12:40:23 -0500

    commit tree 72f3556b2f3887613a2ef000515248d4a522ccb1

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 /*for (let age = 30; age < 40; age++) {
      console.log(`I'm ${age} years old. Happy birthday to me!`);
     
-   }*/ 
-   //const gifts = ["teddy bear", "drone", "doll"];
+   }*/
+//const gifts = ["teddy bear", "drone", "doll"];
 
-   /*function wrapGifts(gifts) {
+/*function wrapGifts(gifts) {
      for (let i = 0; i < gifts.length; i++) {
        console.log(`Wrapped ${gifts[i]} and added a bow!`);
        
@@ -15,18 +15,36 @@
    }
    
    wrapGifts(gifts);*/
-   let thankYou = [];
-   function writeCards(array,event) {
-        for (let i = 0; i < array.length; i++) {
-             thankYou.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
-        }
-        return thankYou;
-   }
-   function countDown(number) {
-        let posNumber = number;
-        while (posNumber > -1) {
-             console.log(posNumber);
-             posNumber--;
-        }
-   }
-   
+//    let thankYou = [];
+//    function writeCards(array,event) {
+//         for (let i = 0; i < array.length; i++) {
+//              thankYou.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+//         }
+//         return thankYou;
+//    }
+//    function countDown(number) {
+//         let posNumber = number;
+//         while (posNumber > -1) {
+//              console.log(posNumber);
+//              posNumber--;
+//         }
+//    }
+
+//new code for review during phase 1
+let thanksArray = [];
+
+function writeCards(array, eventName) {
+  for (let i = 0; i < array.length; i++) {
+    thanksArray.push(
+      `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+  return thanksArray;
+}
+
+function countDown(posInteger) {
+  while (posInteger > -1) {
+    console.log(posInteger);
+    posInteger--;
+  }
+}
